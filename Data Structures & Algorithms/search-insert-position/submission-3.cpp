5f51c3d91316c94ab471d8526ee4bd10211bc5e2
kunal kunal <kunal1624233@gmail.com>
2026-07-11 20:47:08 +0530

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/search-insert-position/submission-3.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int l=0,r=nums.size()-1;
+        int ans=-1;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(nums[mid]>=target){
+                ans=mid;
+                r=mid-1;
+            
+            }else{
+                l=mid+1;
+            }
+        }
+        return ans==-1?nums.size():ans;
+        
+    }
+};
---
+++ Data Structures & Algorithms/sqrtx/submission-3.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+       if(x==0)return 0;
+        int l=1,r=x;
+        
+        int ans=x;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(mid>x/mid){
+                r=mid-1;
+
+            }else{
+                ans=mid;
+                l=mid+1;
+
+            }
+        }
+
+        return ans;
+        
+    }
+};
